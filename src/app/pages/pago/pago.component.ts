b5ef6a231cdf4063293d26a7c6688c1208523575
Daniela Barrientos <daniela@hotmail.com>
2020-10-15 11:33:28 -0500

    disenio de pagina pago

--- src/app/pages/pago/pago.component.ts
+++ src/app/pages/pago/pago.component.ts
@@ -7,9 +7,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagoComponent implements OnInit {
 
+  checkLlevar: Boolean = true;
+  checkRecoger: Boolean = false;
+
+  checkPaypal: boolean = true;
+  checkTarjeta: boolean = false;
+  checkEfectivo: boolean = false;
+
+  envio = {
+    'nombre': 'Hector',
+    'apellidos': 'Nunez',
+    'empresa': 'Kingo System',
+    'sexo': 'H',
+    'telefono': '7444849493',
+    'calle': 'Av. Costera Miguel Aleman',
+    'numExt': '1252',
+    'numInt': '18',
+    'referencia': 'En la torre de Acapulco',
+    'colonia': 'Club Deportivo',
+    'codigoPostal': '39690',
+    'ciudad': 'Acapulco',
+    'estado': 'Guerrero',
+    'pais': 'México',
+  };
+
+  recoger = {
+    'nombre': 'Hector',
+    'apellidos': 'Nunez',
+    'telefono': '7444849493',
+  };
+
+  factura = {
+    'nombre': 'Kingo Systems SA de CV',
+    'rfc': 'KYS010331243',
+    'email': '',
+    'usoDeCEDI': 'G03 - Gastos en general',
+  };
+
+  email: String = "email@ejemplo.com";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  llevarChange(event){
+    console.log(event);
+    this.checkLlevar = event;
+    this.checkRecoger = false;
+  }
+
+  recogerChange(event){
+    this.checkRecoger = event;
+    this.checkLlevar = false;
+  }
+
+  paypalChange(event){
+    this.checkPaypal = event;
+    this.checkTarjeta = false;
+    this.checkEfectivo = false;
+  }
+
+  tarjetaChange(event){
+    this.checkPaypal = false;
+    this.checkTarjeta = event;
+    this.checkEfectivo = false;
+  }
+
+  efectivoChange(event){
+    this.checkPaypal = false;
+    this.checkTarjeta = false;
+    this.checkEfectivo = event;
   }
 
 }
