5ede5e8fbc65e9d434d23923c7d5ce734aa1159d
aDiaz <diaz1517@hotmail.com>
2020-10-14 23:38:41 -0500

    formularios indetificarse

--- src/app/pages/identificarse/identificarse.component.ts
+++ src/app/pages/identificarse/identificarse.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-identificarse',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentificarseComponent implements OnInit {
 
+  passInvalid: boolean = false;
+
+  login = {
+    email: '',
+    password:'',
+  }
+
+  singup = {
+    nombre: '',
+    email:'',
+    password: '',
+    confPassword: '',
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogin(form: NgForm){
+    console.log(form);
+    if(form.invalid){
+      Object.values(form.controls).forEach( controll => controll.markAsTouched());
+      return;
+    }
+  }
+
+  onSingup(form: NgForm){
+    console.log(form)
+    if(form.controls.passwordSingup.value != form.controls.confPasswordSingup.value){
+      this.passInvalid = true;
+      return;
+    }
+    if(form.invalid){
+      Object.values(form.controls).forEach( controll => controll.markAsTouched());
+      return;
+    }else{
+      this.passInvalid = false;
+    }
   }
 
 }
