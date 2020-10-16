b7389c02ab27e336850ef34397b9dc6ae50e6a8a
Daniela Barrientos <daniela@hotmail.com>
2020-10-16 13:30:44 -0500

    agrego variables al carrito del header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { element } from 'protractor';
 import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
@@ -8,6 +9,10 @@ import { ProductosService } from 'src/app/services/productos.service';
 })
 export class HeaderComponent implements OnInit {
 
+  cantidadCarrito: number = 0;
+  totalCarrito:number = 0;
+
+
   categorias;
 
    
@@ -15,6 +20,18 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.categorias = this.productosService.getCategorias();
+    this.cargarCarrito();
+  }
+
+  cargarCarrito(){
+    let carrito = []
+    if(localStorage.getItem('carrito')){
+      carrito = JSON.parse(localStorage.getItem('carrito'));
+      carrito.forEach( element => {
+        this.cantidadCarrito += element.cantidad;
+        this.totalCarrito += element.precio * element.cantidad;
+      });
+    }
   }
 
   
