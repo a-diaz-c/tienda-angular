772486b8dcc627adeb00e033ab07285277e22c81
Daniela Barrientos <daniela@hotmail.com>
2020-10-19 12:40:27 -0500

    datos de carrito en drawer

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -8,12 +8,36 @@ import { ProductosService } from '../services/productos.service';
 })
 export class PagesComponent implements OnInit {
 
+  cantidadCarrito: number = 0;
+  totalCarrito:number = 0;
   categorias;
 
   constructor(private productosService: ProductosService) { }
 
   ngOnInit() {
     this.categorias = this.productosService.getCategorias();
+    this.cargarCarrito();
+    this.actualizarCarrito();
   }
+
+  cargarCarrito(){
+    let carrito = this.productosService.cargarCarrito();
+    carrito.forEach(element => {
+     this.cantidadCarrito += element.cantidad;
+     this.totalCarrito += element.precio * element.cantidad;
+    });
+   }
+ 
+   actualizarCarrito(){
+     this.productosService.getCarrito$().subscribe( carrito => {
+      this.cantidadCarrito = 0;
+      this.totalCarrito = 0;
+      carrito.forEach( element => {
+       this.cantidadCarrito += element.cantidad;
+       this.totalCarrito += element.precio * element.cantidad;
+     });
+       console.log(carrito);
+     });
+   }
 
 }
