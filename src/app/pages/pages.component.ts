55cad1038883097bfdff28a6d34c62b50ddbe9c3
aDiaz <diaz1517@hotmail.com>
2020-10-21 15:05:58 -0500

    muestra lista de familiar sin hijo en drawer

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { element } from 'protractor';
 import { LocationService } from '../services/location.service';
 import { ProductosService } from '../services/productos.service';
 
@@ -7,23 +8,31 @@ import { ProductosService } from '../services/productos.service';
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.css']
 })
-export class PagesComponent implements OnInit {
+export class PagesComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('lista', {static: false}) listaMenu: ElementRef;
 
   cantidadCarrito: number = 0;
   totalCarrito:number = 0;
-  categorias;
+  categorias: any [];
 
   lat;
   lng ;
   
 
-  constructor(private productosService: ProductosService, private locationService: LocationService) { }
+  constructor(private productosService: ProductosService, 
+              private locationService: LocationService,
+              private renderer: Renderer2) { }
 
   ngOnInit() {
     this.categorias = this.productosService.getCategorias();
     this.getLocation();
     this.cargarCarrito();
     this.actualizarCarrito();
+  }
+
+  ngAfterViewInit(){
+    this.listarMenu();
   }
 
   cargarCarrito(){
@@ -51,6 +60,42 @@ export class PagesComponent implements OnInit {
         this.lat = pos.lat;
         this.lng = pos.lng;
         console.log(this.lat  + " - " +  this.lng);
+    });
+  }
+
+  listarMenu(){
+    let ul = this.renderer.createElement('ul');
+    this.renderer.addClass(ul, 'list-group');
+    this.renderer.appendChild(this.listaMenu.nativeElement, ul);
+
+    this.categorias.forEach(element => {
+      let li = this.renderer.createElement('li');
+      this.renderer.setStyle(li, 'list-style', 'none');
+      this.renderer.setStyle(li, 'padding-right', '0px');
+      let button = this.renderer.createElement('button');
+      this.renderer.addClass(button, 'btn');
+      this.renderer.setAttribute(button, 'type','button');
+      this.renderer.setAttribute(button, 'data-toggle', 'collapse');
+      this.renderer.setAttribute(button, 'data-target', '#collapse' + element.id);
+      this.renderer.appendChild(button, this.renderer.createText(element.nombre));
+
+      this.renderer.appendChild(li, button);
+      this.renderer.appendChild(ul, li);
+
+      if(element.hijos.length > 0){
+        let icon = this.renderer.createElement('li');
+        this.renderer.addClass(icon, 'fas');
+        this.renderer.addClass(icon, 'fa-angle-down');
+        this.renderer.appendChild(button,icon);
+
+        let div = this.renderer.createElement('div');
+        this.renderer.addClass(div, 'collapse');
+        this.renderer.setAttribute(div, 'id', 'collapse' + element.id);
+        this.renderer.appendChild(li, div);
+
+        let ul 
+
+      }
     });
   }
 
