ca4587cc89d24482fe98c53831472420044dea8f
Daniela Barrientos <daniela@hotmail.com>
2020-11-06 14:28:27 -0600

    sucursales gruporoca

--- src/app/pages/sucursales/sucursales.component.ts
+++ src/app/pages/sucursales/sucursales.component.ts
@@ -13,11 +13,13 @@ export class SucursalesComponent implements OnInit {
   lng = -101.487698;
   
   marcadores: any[] = [
-    { lat: '18.934104', lng: ' -99.197179', nombre: "Applebee's Cuernavaca", direccion: 'Plaza Carso, Piso 3 Local R02, Col. Granada, C.P. 11529 Ciudad de México, CDMX.' },
-    { lat: '16.860217', lng: '-99.873522', nombre: "Applebee's Acapulco", direccion: 'Villas del Lago, 62374 Cuernavaca, Mor. Cuernavaca. C.P. 62500' },
+    { lat: '16.871539', lng: '-99.813786', nombre: "Acapulco - La Sabana", direccion: 'Lázaro Cárdenas, Ruffo Figueroa, La Sabana, 39799 Acapulco de Juárez, Gro.'},
+    { lat: '17.167920', lng: '-99.534427', nombre: "Tierra Colorada", direccion: ''},
+    { lat: '17.494015', lng: '-99.464983', nombre: "Petaquillas", direccion: ''},
+/*     { lat: '16.860217', lng: '-99.873522', nombre: "Applebee's Acapulco", direccion: 'Villas del Lago, 62374 Cuernavaca, Mor. Cuernavaca. C.P. 62500' },
     { lat: '19.487504', lng: '-99.153238', nombre: "Applebee's Vía Vallejo", direccion: 'Calzada Vallejo, No. 1090, Colonia Santa Cruz de las Salinas, Delegación Azcapotzalco, Ciudad de México, D.F.' },
     { lat: '19.442545', lng: '-99.204091', nombre: "Applebee’s Plaza Carso", direccion: 'Plaza Carso, Piso 3 Local R02, Col. Granada, C.P. 11529 Ciudad de México, CDMX.' },
-    { lat: '25.457571', lng: '-100.979277', nombre: "Applebee´s Galerías Saltillo", direccion: 'Blvd. Nazario S. Ortiz Garza #2345 L-312, Col. Tanque de Peña. Ciudad: Saltillo, Coahuila. CP: 25279 Tel. (844) 485-0596' },
+    { lat: '25.457571', lng: '-100.979277', nombre: "Applebee´s Galerías Saltillo", direccion: 'Blvd. Nazario S. Ortiz Garza #2345 L-312, Col. Tanque de Peña. Ciudad: Saltillo, Coahuila. CP: 25279 Tel. (844) 485-0596' }, */
 
   ]
 
